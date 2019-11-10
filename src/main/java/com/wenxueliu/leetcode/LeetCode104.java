86023972f2743b05d7eff3ef8eed27144b0b8025
wenxueliu <wenxueliu73@163.com>
2019-11-10 10:43:16 +0800

    add LeetCode104

--- src/main/java/com/wenxueliu/leetcode/LeetCode104.java
+++ src/main/java/com/wenxueliu/leetcode/LeetCode104.java
@@ -8,4 +8,24 @@ package com.wenxueliu.leetcode;
  * @modifiedBy ：
  */
 public class LeetCode104 {
+
+    /**
+     * 思路：递归思想
+     * 实现：很简单
+     * 用时：10 分钟
+     * @param root
+     * @return
+     */
+    public int maxDepth(TreeNode root) {
+        return nextDepth(root, 0);
+    }
+
+    public int nextDepth(TreeNode root, int depth) {
+        if (root == null) {
+            return depth;
+        }
+        int leftDepth = nextDepth(root.left, depth+1);
+        int rightDepth = nextDepth(root.right, depth+1);
+        return leftDepth > rightDepth ? leftDepth : rightDepth;
+    }
 }
