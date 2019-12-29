e8273bce41795929a87268033d6e505dc268ee36
wenxueliu <wenxueliu73@163.com>
2019-12-29 13:56:22 +0800

    update leetCode1288

--- src/main/java/com/wenxueliu/leetcode/LeetCode1288.java
+++ src/main/java/com/wenxueliu/leetcode/LeetCode1288.java
@@ -1,6 +1,7 @@
 package com.wenxueliu.leetcode;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 
@@ -67,6 +68,40 @@ public class LeetCode1288 {
         int getEnd() {
             return this.end;
         }
+    }
+
+
+    /**
+     * 思路与我的完全一样，但是代码却比我用 java 8 还精简
+     * @param intervals
+     * @return
+     */
+    public int removeCoveredIntervals1(int[][] intervals) {
+        int len = intervals.length;
+        // 特判
+        if (len < 2) {
+            return len;
+        }
+
+        // 特别注意：当区间左端点相同的时候，右端点降序排序
+        Arrays.sort(intervals, (o1, o2) -> {
+            if (o1[0] == o2[0]) {
+                return o2[1] - o2[0];
+            }
+            return o1[0] - o2[0];
+        });
+
+        // 需要被删除的区间个数
+        int remove = 0;
+        int currentRight = intervals[0][1];
+        for (int i = 1; i < len; i++) {
+            if (intervals[i][1] <= currentRight) {
+                remove++;
+            } else {
+                currentRight = intervals[i][1];
+            }
+        }
+        return len - remove;
     }
 }
 
