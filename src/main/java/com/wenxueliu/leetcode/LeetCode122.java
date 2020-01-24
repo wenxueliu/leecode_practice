4da28cdf796ec98057fce70b39cdce9977434d24
wenxueliu <wenxueliu73@163.com>
2020-01-24 22:50:42 +0800

    add LeetCode122

---
+++ src/main/java/com/wenxueliu/leetcode/LeetCode122.java
@@ -0,0 +1,11 @@
+package com.wenxueliu.leetcode;
+
+/**
+ * @author ： liuwenxue
+ * @date ：2020/01/24 22:48
+ * @description :
+ * @path : com.wenxueliu.leetcode.LeetCode122
+ * @modifiedBy ：
+ */
+public class LeetCode122 {
+}
