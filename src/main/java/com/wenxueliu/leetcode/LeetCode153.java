c7d617bd800770b2af32c9b82dbbcae3ab10b91e
wenxueliu <wenxueliu73@163.com>
2020-08-21 23:33:19 +0800

    update leetcode153

--- src/main/java/com/wenxueliu/leetcode/LeetCode153.java
+++ src/main/java/com/wenxueliu/leetcode/LeetCode153.java
@@ -8,80 +8,6 @@ package com.wenxueliu.leetcode;
  * @modifiedBy ：
  */
 public class LeetCode153 {
-
-    /**
-     * 思路
-     * 遍历过程
-     * 1. 如果 nums[mid] <= num[right] 变化点在 [left,mid] 之间
-     * 2. 如果 nums[mid] > num[right] 变化点在 [mid+1,right] 之间
-     * 结束条件
-     * left > right
-     * @param nums
-     * @return
-     */
-    public int findMin(int[] nums) {
-        int left = 0;
-        int right = nums.length - 1;
-        while (left <= right) {
-            if (nums[left] <= nums[right]) {
-                return nums[left];
-            }
-            int mid = left + ((right - left) >> 1);
-            if (nums[mid] <= nums[right]) {
-                right = mid;
-            } else {
-                left = mid + 1;
-            }
-        }
-        return nums[left];
-    }
-
-    /**
-     * 思路
-     * 遍历过程
-     * 1. 如果 nums[mid] > num[left] 变化点在 mid,right 之间
-     * 2. 如果 nums[mid] < num[left] 变化点在 left,mid 之间
-     * 结束条件
-     * nums[mid-1] > num[mid] 或 nums[mid] > nums[mid+1]
-     * @param nums
-     * @return
-     */
-    public int findMin1(int[] nums) {
-        return findMinIndex1(nums);
-    }
-
-    public int findMinIndex1(int[] nums) {
-        if (nums.length == 1) {
-            return 0;
-        }
-
-        int left = 0;
-        int right = nums.length - 1;
-
-        //本来就是排好序的
-        if (nums[0] < nums[right]) {
-            return 0;
-        }
-
-        while (left <= right) {
-            int mid = left + ((right - left) >> 1);
-            if (nums[mid] < nums[right]) {
-                //结束条件 2
-                if (nums[mid - 1] > nums[mid]) {
-                    return mid;
-                }
-                right = mid - 1;
-            } else {
-                //结束条件 1
-                if (nums[mid] > nums[mid + 1]) {
-                    return mid + 1;
-                }
-                left = mid + 1;
-            }
-        }
-        return -1;
-    }
-
     /**
      * 思路：
      * mid 与 0 比较
@@ -91,11 +17,11 @@ public class LeetCode153 {
      * @param nums 数组
      * @return
      */
-    public int findMin2(int[] nums) {
-        return findMinIndex2(nums);
+    public int findMin1(int[] nums) {
+        return nums[findMinIndex1(nums)];
     }
 
-    int findMinIndex2(int []nums) {
+    int findMinIndex1(int []nums) {
         if (nums.length == 1) {
             return 0;
         }
@@ -105,7 +31,6 @@ public class LeetCode153 {
         if (nums[left] <= nums[right]) {
             return left;
         }
-        int target = 0;
         while (left <= right) {
             int mid = left + (right - left) / 2;
             if (nums[mid] > nums[0]) {
@@ -126,5 +51,75 @@ public class LeetCode153 {
             }
         }
         return -1;
+    }
+
+    /**
+     * 思路
+     * 遍历过程
+     * 1. 如果 nums[mid] > num[left] 变化点在 mid,right 之间
+     * 2. 如果 nums[mid] < num[left] 变化点在 left,mid 之间
+     * 结束条件
+     * nums[mid-1] > num[mid] 或 nums[mid] > nums[mid+1]
+     * @param nums
+     * @return
+     */
+    public int findMin2(int[] nums) {
+        return nums[findMinIndex2(nums)];
+    }
+
+    int findMinIndex2(int []nums) {
+        if (nums.length == 1) {
+            return 0;
+        }
+        int len = nums.length;
+        int left = 0;
+        int right = len - 1;
+        if (nums[left] <= nums[right]) {
+            return left;
+        }
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] >= nums[0]) {
+                //结束条件 1
+                if (nums[mid+1] < nums[mid]) {
+                    return mid+1;
+                }
+                left = mid + 1;
+            } else if (nums[mid] < nums[0]) {
+                //结束条件 2
+                if (nums[mid-1] > nums[mid]) {
+                    return mid;
+                }
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * 思路
+     * 遍历过程
+     * 1. 如果 nums[mid] <= num[right] 变化点在 [left,mid] 之间
+     * 2. 如果 nums[mid] > num[right] 变化点在 [mid+1,right] 之间
+     * 结束条件
+     * left > right
+     * @param nums
+     * @return
+     */
+    public int findMin3(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            if (nums[left] <= nums[right]) {
+                return nums[left];
+            }
+            int mid = left + ((right - left) >> 1);
+            if (nums[mid] <= nums[right]) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return nums[left];
     }
 }
