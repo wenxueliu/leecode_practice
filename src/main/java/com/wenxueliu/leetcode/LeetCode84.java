e86f23cda852d96d412c9b550b363e10bd5004cd
wenxueliu <wenxueliu73@163.com>
2020-09-06 16:51:18 +0800

    update leetcode84

--- src/main/java/com/wenxueliu/leetcode/LeetCode84.java
+++ src/main/java/com/wenxueliu/leetcode/LeetCode84.java
@@ -30,4 +30,34 @@ public class LeetCode84 {
         }
         return maxValue;
     }
+
+    /**
+     * 单调栈 + 特殊情况的考虑
+     * 单调栈 + 哨兵
+     *
+     * 理解该题的方法
+     * 1. [看这个题解](https://leetcode-cn.com/problems/largest-rectangle-in-histogram/solution/xiang-jie-dan-diao-zhan-bi-xu-miao-dong-by-sweetie/)，并自己亲自根据答案画图
+     * 2. [看这个题解](https://leetcode-cn.com/problems/largest-rectangle-in-histogram/solution/bao-li-jie-fa-zhan-by-liweiwei1419/) 深入理解
+     * 3. 自己背写
+     * @param heights
+     * @return
+     */
+    public int largestRectangleArea1(int[] heights) {
+        int len = heights.length;
+        int[]tmp = new int[len + 2];
+        System.arraycopy(heights, 0, tmp, 1, len);
+        Deque<Integer> stack = new ArrayDeque<>();
+        int area = 0;
+        for (int index = 0; index < len + 2; index++) {
+            while (!stack.isEmpty() && tmp[index] < tmp[stack.peek()]) {
+                int rightIndex = index;
+                int heightIndex = stack.pop();
+                int height = tmp[heightIndex];
+                int leftIndex = stack.peek();
+                area = Math.max(area, (rightIndex - leftIndex - 1) * height)
+            }
+            stack.push(index);
+        }
+        return area;
+    }
 }
