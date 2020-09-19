c774ec31ffdc3b0a54c598a16f718198a58c861b
wenxueliu <wenxueliu73@163.com>
2020-09-19 21:41:37 +0800

    add LeetCode901

--- src/main/java/com/wenxueliu/leetcode/LeetCode901.java
+++ src/main/java/com/wenxueliu/leetcode/LeetCode901.java
@@ -1,6 +1,30 @@
-package com.wenxueliu.leetcode;/**
-* @author liuwenxue
-* @date 2020-09-08
-*/
+package com.wenxueliu.leetcode;
+
+import java.util.Stack;
+
+/**
+ * @author liuwenxue
+ * @date 2020-09-08
+ *
+ * 09-18 再次通过，本题 done
+ */
 public class LeetCode901 {
+    Stack<Integer> prices, weights;
+
+    public LeetCode901() {
+        prices = new Stack();
+        weights = new Stack();
+    }
+
+    public int next(int price) {
+        int w = 1;
+        while (!prices.isEmpty() && prices.peek() <= price) {
+            prices.pop();
+            w += weights.pop();
+        }
+
+        prices.push(price);
+        weights.push(w);
+        return w;
+    }
 }
